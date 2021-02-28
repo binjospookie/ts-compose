322d012a87fa8795ca957aae552cee54ca5385bf
Viktor Pasynok <binjospookie@gmail.com>
2021-02-28 18:32:43 +0300

    docs: improve

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # ts-compose
+
+```
+((y → z), (x → y), …, (o → p), ((a, b, …, n) → o)) → ((a, b, …, n) → z)
+```
+
+Performs right-to-left function composition.
+
+```typescript
+import { compose } from 'ts-compose';
+
+const addOne = (x: number) => x + 1;
+const double = (x: number) => x * 2;
+const toUpper = (x: string) => x.toUpperCase();
+const fold = (x, y) => x + y;
+const classyGreeting = (firstName: string, lastName: string) => "The name's " + lastName + ", " + firstName + " " + lastName;
+const yellGreeting = compose(toUpper, classyGreeting);
+
+compose(Math.abs, addOne, double)(-4); //=> 7
+yellGreeting('James', 'Bond'); //=> "THE NAME'S BOND, JAMES BOND"
+
+compose(double, addOne, addOne, toUpper)('1'); //=> can't compose toUpper with addOne
+compose(double, addOne, addOne, fold)(1); //=> expect 2 arguments
+```
+
+[Try it](https://codesandbox.io/s/ts-compose-4c8fh?file=/src/index.ts)
+
--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -3,18 +3,25 @@ const { compose } = require('../../build/index');
 const addOne = (x) => x + 1;
 const double = (x) => x * 2;
 const fold = (x, y) => x + y;
-const uppercase = (x) => x.toUpperCase();
+const toUpper = (x) => x.toUpperCase();
 
 const baseFn = compose(double, addOne, addOne);
 
 describe('compose', () => {
+  it('example', () => {
+    const classyGreeting = (firstName, lastName) => "The name's " + lastName + ', ' + firstName + ' ' + lastName;
+    const yellGreeting = compose(toUpper, classyGreeting);
+    expect(yellGreeting('James', 'Bond')).toBe("THE NAME'S BOND, JAMES BOND");
+
+    expect(compose(Math.abs, addOne, double)(-4)).toBe(7);
+  });
   it('happy', () => {
     expect(compose(double, addOne, addOne)(1)).toBe(6);
     expect(compose(baseFn, fold)(1, 2)).toBe(10);
-    expect(compose(baseFn, uppercase)('test')).toBe(NaN);
+    expect(compose(baseFn, toUpper)('test')).toBe(NaN);
   });
   it('error', () => {
-    const fn = compose(baseFn, uppercase);
+    const fn = compose(baseFn, toUpper);
     try {
       fn(1);
     } catch ({ message }) {
