586b11a3c1810a328688d73db7cca36e33fac956
Viktor Pasynok <binjospookie@gmail.com>
2021-02-28 18:07:59 +0300

    tests: add

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -5,10 +5,20 @@ const double = (x) => x * 2;
 const fold = (x, y) => x + y;
 const uppercase = (x) => x.toUpperCase();
 
+const baseFn = compose(double, addOne, addOne);
+
 describe('compose', () => {
   it('happy', () => {
     expect(compose(double, addOne, addOne)(1)).toBe(6);
-
-    expect(compose(double, addOne, addOne, fold)(1, 2)).toBe(10);
+    expect(compose(baseFn, fold)(1, 2)).toBe(10);
+    expect(compose(baseFn, uppercase)('test')).toBe(NaN);
+  });
+  it('error', () => {
+    const fn = compose(baseFn, uppercase);
+    try {
+      fn(1);
+    } catch ({ message }) {
+      expect(message).toBe('x.toUpperCase is not a function');
+    }
   });
 });
